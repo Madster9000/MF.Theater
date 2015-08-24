080c8a0d8d843156422e6e21fbd3f51349d01be8
HomeDevelop\Madster <ikovalenko9000@gmail.com>
2015-08-24 23:11:20 +0200

    UI added

---
+++ MF.Theater.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MF.Theater.UI.Startup))]
+namespace MF.Theater.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
